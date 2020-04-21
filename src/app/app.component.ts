0961f359cb995ca29a2a1bbd2b159b303e763348
a121 <shankar.pisal@codevian.com>
2020-04-21 13:25:23 +0530

    added new charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,136 @@ import { Chart } from "chart.js";
 })
 export class AppComponent implements OnInit {
   title = 'Charts';
-  LineChart;
-  PieChart;
-  BarChart;
+  LineChart : any;
+  PieChart : any;
+  BarChart : any;
+  LineCharts : any;
+  lineChartDataSet : any;
+  lineOptions : any;
+
+  constructor(){
+    const totalRecs = {
+      label: "Total Recs",
+      fill: "false",
+      lineTension: "0",
+      borderColor: "#FF0000",
+      borderWidth: 1,
+      data: [0, 60, 68, 52, 50, 51]
+    };
+    const fillRate = {
+      label: "Fill Rate",
+      fill: "false",
+      yAxisID: "percent",
+      lineTension: "0",
+      borderColor: "#8FAADC",
+      borderWidth: 1,
+      data: [29, 25, 24, 33, 20, 10]
+    };
+    const fillRate2 = {
+      label: "Fill Rate2",
+      fill: false,
+      type: "bar",
+      yAxisID: "percent",
+      lineTension: "0",
+      borderColor: "#000",
+      backgroundColor: "#00b0f0",
+      borderWidth: 1,
+      data: [20, 10, 4, 1, 15, 5]
+    };
+    const AvgFillRate = {
+      label: "Avg Fill Rate in Days",
+      fill: "false",
+      lineTension: "0",
+      borderColor: "#5B9BD5",
+      borderWidth: 1,
+      data: [10, 8, 5, 7, 11, 6]
+    };
+    const AvgFillRate2 = {
+      label: 'Number of Items Sold in Months',
+      data: [29, 7, 23, 15, 20, 10],
+      yAxisID: "percent",
+      fill: true,
+      backgroundColor : "#e3f2f2",
+      lineTension: 0.3,
+      borderColor: "#000",
+      borderWidth: 1
+    }
+    this.lineChartDataSet = {
+      labels: ["Jan", "Feb", "March", "April", "May", "June"],
+      datasets: [totalRecs, AvgFillRate, fillRate,AvgFillRate2,fillRate2]
+    };
+    this.lineOptions = {
+      legend: {
+        position: "top",
+        labels: {
+          // fontFamily: "Comic Sans MS",
+          boxWidth: 30,
+          boxHeight: 0,
+          fontSize: 10
+        },
+        maxSize: {
+          height: 0
+        }
+      },
+      title: {
+        text: "Aggregate Recruiter Performance",
+        display: true
+      },
+      scales: {
+        ticks: {
+          beginAtZero: true
+        },
+        xAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+              display: false
+            }
+          }
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+              display: false
+            }
+          },
+          {
+            id: "percent",
+            type: "linear",
+            position: "right",
+
+            ticks: {
+              max: 35,
+              min: 0,
+              callback: function(value, index, values) {
+                return value + "%";
+              }
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, 0)"
+            }
+          }
+        ]
+      },
+      elements: {
+        line: {
+          tension: 0
+        },
+        point: {
+          radius: 0
+        }
+      },
+      bezierCurve: false
+    };
+  }
+
   ngOnInit() {
     // Line chart:
     this.LineChart = new Chart('lineChart', {
@@ -113,6 +240,7 @@ export class AppComponent implements OnInit {
         }]
       },
       options: {
+        responsive : true,
         title: {
           text: "Bar Chart",
           display: true
@@ -126,6 +254,12 @@ export class AppComponent implements OnInit {
           }]
         }
       }
+    });
+
+    this.LineCharts = new Chart("lineCharts", {
+      type: "line",
+      data: this.lineChartDataSet,
+      options: this.lineOptions
     });
 
   }
